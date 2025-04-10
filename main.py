c9f0bd04526e8086806d194be5cca6c23ab97c06
beandrea <arkflame99@gmail.com>
2025-04-10 06:03:56 -0400

    working automod for txt msgs

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 from dotenv import load_dotenv
 import os
 import discord
+from discord import Intents
+
+banned_list = []
+strikes_list = {}
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client(intents=Intents.all())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if '!words add' in message.content:
+        words = message.content[10:].split()
+        for word in words:
+            banned_list.append(word)
+            await message.channel.send(f'{word} added to banned words list')
+
+    if message.content == '!words list':
+        await message.channel.send(banned_list)
+
+    if message.content in banned_list:
+        if message.author.name not in strikes_list:
+            strikes_list[message.author.name] = 0
+        strikes_list[message.author.name] += 1
+        await message.channel.send(f'{message.author.name} has been given a strike')
+        if strikes_list[message.author.name] < 3:
+            pass
+        elif strikes_list[message.author.name] == 3:
+            ban(message.author)
+        await message.delete()
+
+async def ban(member : discord.Member):
+    await member.ban()
+
+client.run(TOKEN)
