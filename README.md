edefd4f27d73152ddd557a090461f9c380d52463
beandrea <arkflame99@gmail.com>
2025-04-10 14:04:59 -0400

    i hope this is fully working

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # discordModBot
 * Add words to a ban list with '!words add', then if a member's message contains those words, the message is automatically deleted and given a strike.
-  * Each strike, until the last will come with a timeout, though (coming soon) the punishments for a strike and maximum strikes will be editable
-  * If a member is given N strikes, where N is defaulted to 3, but setable with '!words set strikes', they will be banned (editable, coming soon) from the server.
+  * Each strike, until the last will come with a timeout, though the punishments for strikes and maximum strikes are hopefully editable
+  * If a member is given N strikes, where N is defaulted to 3, but setable with '!words set strikes', they will be banned (editing this should hopefully work) from the server.
   * The banned words list is also viewable with '!words list' and editable (coming soon)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import discord
+import datetime
 from discord import Intents
 from dotenv import load_dotenv
-from time import time
 
 banned_list = []
 strikes_list = {}
 max_strikes = 3
-timeout = 1
+time_out = 1
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,10 +19,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
+    global max_strikes
+    global time_out
+
     if message.author == client.user:
         return
 
-    if '!words add' in message.content:
+    if message.content == '!myperms':
+        perms = message.channel.guild.me.guild_permissions
+        formatted = [f"✅ {name}" if value else f"❌ {name}" for name, value in perms]
+        perm_list = "\n".join(formatted)
+
+        embed = discord.Embed(
+            title=f"Permissions for {client.user.name}",
+            description=perm_list,
+            color=discord.Color.blue())
+
+        await message.channel.send(embed=embed)
+
+    if '!words add ' in message.content:
         words = message.content[10:].split()
         for word in words:
             banned_list.append(word)
@@ -31,37 +46,49 @@ async def on_message(message: discord.Message):
     if message.content == '!words list':
         await message.channel.send(banned_list)
 
-    if '!words set strikes' in message.content:
+    if '!words set strikes ' in message.content:
         max_strikes = (int)(message.content[17:])
 
-    if '!actions' in message.content:
-        if 'edit strikes' in message.content[8:]:
-            pass
-        if 'edit max strikes' in message.content[8:]:
-            pass
+    if '!actions ' in message.content:
+        msg = message.content
+        channel = message.channel
+        if 'edit strikes ' in msg[8:] and 'edit max strikes ' not in msg[8:]:
+            if 'days' in msg or 'day' in msg:
+                end = msg.index('day')
+                time_out = ((int)(msg[21: end])) * 24
+            if 'hrs' in msg or 'hr' in msg:
+                end = msg.index('hr')
+                time_out = ((int)(msg[21: end]))
+            if 'mins' in msg or 'min' in msg:
+                end = msg.index('min')
+                time_out = ((int)(msg[21: end])) / 60
+            await channel.send(f'Timeout set to {msg[21:]}')
+        if 'edit max strikes ' in msg[8:]:
+            punishments['strikes_max'] = msg[25:]
+            await channel.send(f'Max strike punishment set to {msg[25:]}')
 
     if message.content in banned_list:
         if message.author.name not in strikes_list:
             strikes_list[message.author.name] = 0
         strikes_list[message.author.name] += 1
         await message.channel.send(f'{message.author.name} has been given a strike')
-        if strikes_list[message.author.name] < max_strikes:
-            action = punishments['strikes']
-            handle(action, message.author)
-        elif strikes_list[message.author.name] == max_strikes:
+
+        if strikes_list[message.author.name] == max_strikes:
             action = punishments['strikes_max']
-            handle(action, message.author)
+            await handle(action, message.author)
         await message.delete()
 
 async def handle(action, member: discord.Member):
-    action(member)
+    await action(member)
 
 async def ban(member: discord.Member):
     await member.ban()
 
+async def kick(member: discord.Member):
+    await member.kick()
+
 async def timeout(member: discord.Member):
-    currTime = round(time() * 1000)
-    await member.timeout(currTime + (timeout * 3600000))
+    await member.timeout(datetime.timedelta(milliseconds=(time_out * 3600000)))
 
 punishments = {
     'strikes': timeout,
